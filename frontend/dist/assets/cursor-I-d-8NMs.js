9e4cf6f11bc37513f3934811ff7ecc2c131860e7
Danilo <foconascriptos@gmail.com>
2026-01-11 20:53:26 -0300

    chore: trigger rebuild with trace marker

---
+++ frontend/dist/assets/cursor-I-d-8NMs.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-Dv2571JN.js";import"./index-DMxCxrW9.js";import"./events-2gcfCWYN.js";import"./index.es-BvuRrCvO.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
